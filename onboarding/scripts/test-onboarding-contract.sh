8cacc3177fdc9721929584b95fa121c50b7c129c
hiraoku.shinichi <s.hiraoku@gmail.com>
2026-07-30 09:07:00 +0900

    Fix onboarding contract verifier fixture

--- onboarding/scripts/test-onboarding-contract.sh
+++ onboarding/scripts/test-onboarding-contract.sh
@@ -134,7 +134,11 @@ mutate_wip_limit() {
 }
 
 mutate_verifier() {
-  sed -i.bak 's/enabled: true/enabled: false/' "$1/.kaizen/config.yml"
+  sed -i.bak \
+    -e 's/^safety:$/safety:\
+  operationMode: dogfood/' \
+    -e 's/enabled: true/enabled: false/' \
+    "$1/.kaizen/config.yml"
   rm "$1/.kaizen/config.yml.bak"
 }
 
